04a9dc18d0e0719d0ca6983c9d6a3f39246bb6d4
sristysingh.ss <sristysingh.ss97@gmail.com>
2026-01-07 09:45:29 +0530

    SignIn/SignUp form

---
+++ src/components/Browse.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Browse = () => {
+  return <div className="bg-red-600 w-fit my-0 mx-auto">Hi</div>;
+};
+
+export default Browse;
---
+++ src/components/Login.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import Header from "./Header";
+
+const Login = () => {
+  const [isSignIn, setIsSignIn] = useState(true);
+  const toggleSignIn = () => {
+    setIsSignIn(!isSignIn);
+  };
+  return (
+    <div className="">
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/d13e2d55-5cdd-48c0-a55b-4b292d0b9889/web/IN-en-20251229-TRIFECTA-perspective_d7edcd70-4cfd-441c-858c-c5e400ed6c2b_large.jpg"
+          srcSet="https://assets.nflxext.com/ffe/siteui/vlv3/d13e2d55-5cdd-48c0-a55b-4b292d0b9889/web/IN-en-20251229-TRIFECTA-perspective_d7edcd70-4cfd-441c-858c-c5e400ed6c2b_large.jpg 2000w,
+          https://assets.nflxext.com/ffe/siteui/vlv3/d13e2d55-5cdd-48c0-a55b-4b292d0b9889/web/IN-en-20251229-TRIFECTA-perspective_d7edcd70-4cfd-441c-858c-c5e400ed6c2b_medium.jpg 1279w,
+          https://assets.nflxext.com/ffe/siteui/vlv3/d13e2d55-5cdd-48c0-a55b-4b292d0b9889/web/IN-en-20251229-TRIFECTA-perspective_d7edcd70-4cfd-441c-858c-c5e400ed6c2b_small.jpg 959w"
+          alt="bg"
+          aria-hidden="true"
+        ></img>
+      </div>
+      <form className="w-1/3 bg-black bg-opacity-80 mt-36 absolute left-1/2 -translate-x-1/2 text-white p-10 rounded-md">
+        <h1 className="font-bold text-3xl">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignIn && (
+          <input
+            placeholder="Name"
+            type="text"
+            className="p-4 my-3 w-full rounded-md"
+          />
+        )}
+        <input
+          placeholder="Email or Phone Number"
+          type="text"
+          className="p-4 my-3 w-full rounded-md"
+        />
+        <input
+          placeholder="Password"
+          type="text"
+          className="p-4 my-3 w-full rounded-md"
+        />
+        <button className="bg-red-700 p-4 my-3 w-full rounded-md">
+          {isSignIn ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignIn}>
+          {isSignIn
+            ? "New to Netflix? Sign Up Now"
+            : "Already have an account? Sign In Now"}
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
